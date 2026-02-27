25f480b149e6ca4c18fde235d6e4b9da5df843a8
Nakib <nakib.m@thecamp.fr>
2026-02-27 09:54:38 +0100

    config refresh token

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,46 +2,76 @@
 
 namespace App\Entity;
 
-use App\Repository\UserRepository;
+use App\Enum\Role;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping\HasLifecycleCallbacks;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\ORM\Mapping\PrePersist;
+use Doctrine\ORM\Mapping\PreUpdate;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\HasLifecycleCallbacks]
 #[ORM\Table(name: '`user`')]
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: 'L\'email est obligatoire')]
+    #[Assert\Email(message: 'L\'email {{ value }} n\'est pas valide.')]
+    #[ORM\Column(length: 180, unique: true)]
     private ?string $email = null;
 
-    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: 'Le mot de passe est obligatoire')]
+    #[Assert\Length(min: 8, minMessage: 'Votre mot de passe doit faire au moins {{ limit }} caractères')]
+    #[ORM\Column(type: "string", length: 255)]
     private ?string $password = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $roles = null;
+    #[ORM\Column(enumType: Role::class, name: "role")]
+    private Role $role = Role::USER;
 
-    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    #[ORM\Column(length: 50)]
     private ?string $firstname = null;
 
-    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    #[ORM\Column(length: 50)]
     private ?string $lastname = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, unique: true)]
+    #[Assert\Length(min: 3)]
+    #[Assert\NotBlank]
     private ?string $userName = null;
 
-    #[ORM\Column]
+    #[ORM\Column(type: 'datetime_immutable')]
     private ?\DateTimeImmutable $createdAt = null;
 
-    #[ORM\Column]
+    #[ORM\Column(type: 'datetime_immutable')]
     private ?\DateTimeImmutable $updatedAt = null;
 
     public function getId(): ?int
     {
         return $this->id;
     }
+
+    public function eraseCredentials(): void
+    {
+        // Implement the required method
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email; 
+    }
+
 
     public function setId(int $id): static
     {
@@ -74,15 +104,25 @@ class User
         return $this;
     }
 
-    public function getRoles(): ?string
+    public function getRoles(): array
     {
-        return $this->roles;
+        $roles = ['ROLE_USER'];
+
+        if ($this->role instanceof Role) {
+            $roles[] = $this->role->value;
+        }
+
+        return array_unique($roles);
     }
 
-    public function setRoles(string $roles): static
+    public function getRole(): ?Role
     {
-        $this->roles = $roles;
+        return $this->role;
+    }
 
+    public function setRole(Role $role): static
+    {
+        $this->role = $role;
         return $this;
     }
 
